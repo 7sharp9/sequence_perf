ce7241554db58211a60750b3cc6211700a452f26
Dave Thomas <kukulcanenator@googlemail.com>
2020-02-20 22:06:30 +0000

    Add 10x tests

--- benches/tests.rs
+++ benches/tests.rs
@@ -5,15 +5,37 @@ extern crate test;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use sequence_perf::{sequence_idiomatic, sequence, sequence2, sequence_by_row, sequence_by_column, sequence_by_rc};
+    use sequence_perf::*;
     use itertools::Itertools;
     use test::{Bencher, black_box};
+
+    fn all3()  -> Vec<Vec<u32>> {
+        let x = vec![1, 2, 3, 4];
+        let y = vec![10, 20, 30, 40];
+        let z = vec![100, 200, 300, 400];
+        vec![x, y, z]
+    } 
+
+    fn all4()  -> Vec<Vec<u32>> {
+        let x = vec![1, 2, 3, 4];
+        let y = vec![10, 20, 30, 40];
+        let z = vec![100, 200, 300, 400];
+        let w = vec![1000, 2000, 3000, 4000];
+        vec![x, y, z, w]
+    }
 
     fn all()  -> Vec<Vec<u32>> {
         let x = vec![1, 2, 3, 4];
         let y = vec![10, 20, 30, 40];
         let z = vec![100, 200, 300, 400];
-        vec![x, y, z]
+        let w = vec![1000, 2000, 3000, 4000];
+        let a = vec![10000, 20000, 30000, 40000];
+        let b = vec![10000, 20000, 30000, 40000];
+        let c = vec![10000, 20000, 30000, 40000];
+        let d = vec![10000, 20000, 30000, 40000];
+        let e = vec![10000, 20000, 30000, 40000];
+        let f = vec![10000, 20000, 30000, 40000];
+        vec![x, y, z, w, a, b,c ,d, e, f]
     } 
 
     #[bench]
@@ -71,4 +93,14 @@ mod tests {
             sequence_by_rc(&all)
         })
     }
+
+    #[bench]
+    fn sequence_by_row_reyon_test(b: &mut Bencher) {
+        let all = all();
+        b.iter(|| {
+            sequence_by_row_rayon(&all)
+        })
+    }
+
+    
 }
